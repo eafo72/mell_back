824cba5a4dea1cb9ab6aebff6bf726cffa232699
alex <eafo72@gmail.com>
2023-11-22 10:07:36 -0600

    22112023

--- routes/users.js
+++ routes/users.js
@@ -83,15 +83,15 @@ app.post('/login', async (req, res) => {
 	const { email, password } = req.body
 
 	try {
-		let foundUser = await Usuario.findOne({ email:email})
+		let foundUser = await Usuario.findOne({ correo:email})
         if(!foundUser){
-            return res.status(400).json({msg:'El usuario no existe'}) 
+            return res.status(500).json({msg:'El usuario no existe'}) 
         }
 
         const passCorrecto = await bcryptjs.compare(password,foundUser.password)
 
         if(!passCorrecto){
-            return await res.status(400).json({msg: 'Password incorrecto'})
+            return await res.status(500).json({msg: 'Password incorrecto'})
         }
 
         const payload = {
@@ -105,13 +105,13 @@ app.post('/login', async (req, res) => {
             jwt.sign(payload, process.env.SECRET, {expiresIn:3600000}, (error,token)=>{
                 if(error) throw error
 
-                res.json({token})
+                res.status(200).json({token})
             })
         }else{
-            res.json({msg:'Hubo un error', error})
+            res.status(500).json({msg:'Hubo un error', error})
         }
 	} catch (error) {
-		res.json({ msg: 'Hubo un error', error })
+		res.status(500).json({ msg: 'Hubo un error', error })
 	}
 })
 
