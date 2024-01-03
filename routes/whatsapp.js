3e8f78cd0761363d5f68886df307a4c6d64fd96c
alex <eafo72@gmail.com>
2024-01-03 13:37:21 -0600

    03012024

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -2,152 +2,159 @@ const express = require("express");
 const app = express.Router();
 const https = require("https");
 
-function EnviarMensajeWhastpapp(texto,number){
-
+function EnviarMensajeWhastpapp(texto, number) {
   texto = texto.toLowerCase();
   let data = "";
 
-  if (texto.includes("hola")){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "text",
-          "text": {
-              "preview_url": false,
-              "body": "🚀 Hola, Como estas, Bienvenido."
-          }
-      });
-  }else if (texto=="1"){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "text",
-          "text": {
-              "preview_url": false,
-              "body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-          }
-      });
-  }else if(texto=="2"){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "to": number,
-          "type": "location",
-          "location": {
-          "latitude": "-12.067158831865067",
-          "longitude": "-77.03377940839486",
-          "name": "Estadio Nacional del Perú",
-          "address": "Cercado de Lima"
-          }
-      });
-  }else if(texto=="3"){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "document",
-          "document": {
-              "link": "http://jornadasciberseguridad.riasc.unileon.es/archivos/ejemplo_esp.pdf",
-              "caption": "Temario del Curso #001"
-          }
-      });
-  }else if(texto=="4"){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "audio",
-          "audio": {
-              "link": "https://filesamples.com/samples/audio/mp3/sample1.mp3"
-          }
-      });
-  }else if(texto=="5"){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "to": number,
-          "text": {
-              "preview_url": true,
-              "body": "Introduccion al curso! https://youtu.be/6ULOE2tGlBM"
-          }
-      });
-  }else if(texto=="6"){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "text",
-          "text": {
-              "preview_url": false,
-              "body": "🤝 En breve me pondre en contacto contigo. 🤓"
-          }
-      });
-  }else if(texto=="7"){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "text",
-          "text": {
-              "preview_url": false,
-              "body": "📅 Horario de Atención : Lunes a Viernes. \n🕜 Horario : 9:00 am a 5:00 pm 🤓"
-          }
-      });
-  }else if(texto.includes("gracias")){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "text",
-          "text": {
-              "preview_url": false,
-              "body": "Gracias a ti por contactarme. 🤩"
-          }
-      });
-  }else if(texto.includes("adios") || texto.includes("bye") || texto.includes("nos vemos")){
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "text",
-          "text": {
-              "preview_url": false,
-              "body": "Hasta luego. 🌟"
-          }
-      });
-  }else{
-      data = JSON.stringify({
-          "messaging_product": "whatsapp",
-          "recipient_type": "individual",
-          "to": number,
-          "type": "text",
-          "text": {
-              "preview_url": false,
-              "body": "🚀 Hola, visita mi web anderson-bastidas.com para mas información.\n \n📌Por favor, ingresa un numero #️⃣ para recibir información.\n \n1️⃣. Información del Curso. ❔\n2️⃣. Ubicación del local. 📍\n3️⃣. Enviar temario en pdf. 📄\n4️⃣. Audio explicando curso. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con AnderCode. 🙋‍♂️\n7️⃣. Horario de Atención. 🕜"
-          }
-      });
+  if (texto.includes("hola")) {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "text",
+      text: {
+        preview_url: false,
+        body: "🚀 Hola, Como estas, Bienvenido.",
+      },
+    });
+  } else if (texto == "1") {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "text",
+      text: {
+        preview_url: false,
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      },
+    });
+  } else if (texto == "2") {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      to: number,
+      type: "location",
+      location: {
+        latitude: "-12.067158831865067",
+        longitude: "-77.03377940839486",
+        name: "Estadio Nacional del Perú",
+        address: "Cercado de Lima",
+      },
+    });
+  } else if (texto == "3") {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "document",
+      document: {
+        link: "http://jornadasciberseguridad.riasc.unileon.es/archivos/ejemplo_esp.pdf",
+        caption: "Temario del Curso #001",
+      },
+    });
+  } else if (texto == "4") {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "audio",
+      audio: {
+        link: "https://filesamples.com/samples/audio/mp3/sample1.mp3",
+      },
+    });
+  } else if (texto == "5") {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      to: number,
+      text: {
+        preview_url: true,
+        body: "Introduccion al curso! https://youtu.be/6ULOE2tGlBM",
+      },
+    });
+  } else if (texto == "6") {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "text",
+      text: {
+        preview_url: false,
+        body: "🤝 En breve me pondre en contacto contigo. 🤓",
+      },
+    });
+  } else if (texto == "7") {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "text",
+      text: {
+        preview_url: false,
+        body: "📅 Horario de Atención : Lunes a Viernes. \n🕜 Horario : 9:00 am a 5:00 pm 🤓",
+      },
+    });
+  } else if (texto.includes("gracias")) {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "text",
+      text: {
+        preview_url: false,
+        body: "Gracias a ti por contactarme. 🤩",
+      },
+    });
+  } else if (
+    texto.includes("adios") ||
+    texto.includes("bye") ||
+    texto.includes("nos vemos")
+  ) {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "text",
+      text: {
+        preview_url: false,
+        body: "Hasta luego. 🌟",
+      },
+    });
+  } else {
+    data = JSON.stringify({
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to: number,
+      type: "text",
+      text: {
+        preview_url: false,
+        body: "🚀 Hola, visita mi web anderson-bastidas.com para mas información.\n \n📌Por favor, ingresa un numero #️⃣ para recibir información.\n \n1️⃣. Información del Curso. ❔\n2️⃣. Ubicación del local. 📍\n3️⃣. Enviar temario en pdf. 📄\n4️⃣. Audio explicando curso. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con AnderCode. 🙋‍♂️\n7️⃣. Horario de Atención. 🕜",
+      },
+    });
   }
 
   const options = {
-      host : "https://graph.facebook.com",
-      path : "/v17.0/197145163483349/messages",
-      method : "POST",
-      body : data,
-      headers : {
-          "Content-Type" : "application/json",
-          Authorization :"Bearer EAATBOAU5ZA6QBO0lNkXwjZA2J7HqszMKUtZCdZBW4s7meoycx2GS4r9JqXnqIgGvbZAEu5UyZBcSRj24AK60zSjczYcQE1WXEVo76EOqLg1SE08cSuD9IDTUN3ABKftxGO6ZCJPyNUnCWnyJ8zwZAcdj13ZCkIo8ZC2nmjEdUvoqJrfFsBKLZAGoaZCzQusDVmZBEudNWxqQM8PQFFKkvV0ZAqNdyVy7nHsEJbrikFL80ZD"
-      }
+    host: "graph.facebook.com",
+    path: "/v17.0/197145163483349/messages",
+    method: "POST",
+    body: data,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer EAATBOAU5ZA6QBO0lNkXwjZA2J7HqszMKUtZCdZBW4s7meoycx2GS4r9JqXnqIgGvbZAEu5UyZBcSRj24AK60zSjczYcQE1WXEVo76EOqLg1SE08cSuD9IDTUN3ABKftxGO6ZCJPyNUnCWnyJ8zwZAcdj13ZCkIo8ZC2nmjEdUvoqJrfFsBKLZAGoaZCzQusDVmZBEudNWxqQM8PQFFKkvV0ZAqNdyVy7nHsEJbrikFL80ZD",
+    },
   };
 
-  //console.log(data);
-  const req = https.request(options,res => {
-      res.on("data",d=>{
-          process.stdout.write(d);
+  try {
+    const req = https.request(options, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d);
       });
-  });
+    });
 
-  req.write(data);
-  req.end();
+    req.write(data);
+    req.end();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 app.post("/", (req, res) => {
@@ -165,7 +172,6 @@ app.post("/", (req, res) => {
 
       //console.log(texto);
       //console.log(numero);
-
 
       EnviarMensajeWhastpapp(texto, numero);
     }
