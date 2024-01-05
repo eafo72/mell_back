0eba6c198fc9175fd115b66c8cc9accf697fb935
alex <eafo72@gmail.com>
2024-01-05 17:33:36 -0600

    05012024

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -225,4 +225,15 @@ app.get("/", (req, res) => {
   }
 });
 
+
+// LISTA
+app.get("/obtener", async (req, res) => {
+  try {
+    const mensajes = await Mensaje.find({});
+    res.json({ mensajes });
+  } catch (error) {
+    res.status(500).json({ msg: "Hubo un error obteniendo los datos" });
+  }
+});
+
 module.exports = app;
