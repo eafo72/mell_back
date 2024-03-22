5d65ff3a738e76c83e22b15f77c842a92e180885
alex <eafo72@gmail.com>
2024-03-22 13:46:11 -0600

    22032024

--- routes/order.js
+++ routes/order.js
@@ -53,6 +53,75 @@ app.get('/single/:id', async (req, res) => {
 })
 
 
+// Checar si hay stock suficiente para surtir el pedido
+app.post('/checkStock', async (req, res) => {
+	const { 
+		tipo_venta,
+        subtotal,
+        descuento,
+        iva,
+        total,
+        descripcion,
+        usuario,
+        correo,
+		entregar_a,
+        direccion_entrega,
+		costo_envio,
+        telefono,
+        estatus_pago,
+        estatus_envio,
+        vendedor,
+		forma_entrega,
+        forma_pago,
+        num_parcialidades,
+        parcialidades
+	} = req.body 
+
+	
+	try {
+
+		//primero revisamos si hay existencia de cada producto
+		for(let i=0; i < descripcion.length; i++){
+
+			//primer filtro si existe el codigo y hay stock pero sin considerar aun los apartados			
+			const ifAvailableOne = await Stock.find({ codigo: descripcion[i]['codigo'], stock: {$gte : descripcion[i]['cantidad'] } });
+			  
+			if (ifAvailableOne.length == 0) {
+				res.json({
+					disponible: false,
+					msg: "El producto " + descripcion[i]['codigo'] + " no tiene el stock necesario para este pedido",
+				})
+				return;
+			}
+
+			//aqui faltaria que recorriera todo el arreglo de productos ya que puede haber en varios almacenes
+			//segundo filtro calcular stock - apartado y ver si alcanza
+			const calculo = (ifAvailableOne[0].stock - ifAvailableOne[0].apartado) -  descripcion[i]['cantidad'];
+						  
+			if (calculo < 0) {
+				res.json({
+					disponible: false,
+					msg: "El producto " + descripcion[i]['codigo'] + " no tiene el stock necesario para este pedido",
+				})
+				return;
+			}
+		}
+
+		res.json({
+			disponible: true,
+			msg: "Stock suficiente",
+		})
+		
+	} catch (error) {
+		res.status(500).json({
+			msg: 'Hubo un error revisando los datos'+error,
+		})
+	}
+})
+
+
+
+
 // CREAR
 app.post('/crear', async (req, res) => {
 	const { 
@@ -90,6 +159,8 @@ app.post('/crear', async (req, res) => {
 	try {
 
 
+		/*
+		ya revisamos stock en el endpoint de arriba antes de comprar
 		//primero revisamos si hay existencia de cada producto
 		for(let i=0; i < descripcion.length; i++){
 
@@ -115,7 +186,7 @@ app.post('/crear', async (req, res) => {
 				return;
 			}
 		}
-
+		*/
 
 		const nuevoPedido = await Pedidos.create({
 			usuario,
